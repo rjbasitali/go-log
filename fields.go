6c5b15c7019ccbe091a9ede96bfd6175f63fd1ad
rjbasitali <rjbasitali@gmail.com>
2022-06-25 02:47:13 +0500

    fields map reference issue

--- fields.go
+++ fields.go
@@ -2,9 +2,14 @@ package log
 
 func (l myLogger) WithFields(fields Fields) Logger {
 	if l.data != nil && len(l.data) > 0 {
-		for k, v := range fields {
-			l.data[k] = v
+		f := Fields{}
+		for k, v := range l.data {
+			f[k] = v
 		}
+		for k, v := range fields {
+			f[k] = v
+		}
+		l.data = f
 	} else {
 		l.data = fields
 	}
